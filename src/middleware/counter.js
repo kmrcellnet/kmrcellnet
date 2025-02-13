a90759947b4b5e2048ed6accf83055cd63ebf6e5
kmrcellnet <bangkomar232@gmail.com>
2025-02-13 22:41:40 +0700

    Update counter.js

--- src/middleware/counter.js
+++ src/middleware/counter.js
@@ -1,1 +1,12 @@
+const addCounter = async (req, res, next) => {
+  try {
+    await fetch(
+      "https://vercel.com/kmrcellnets-projects/web/D9ttPExij34HX6j1kwHVNeF3QmYn"
+    );
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+export default addCounter;
