675f9aaa055d5404ce123889f80cc657e6433ee9
kmrcellnet <bangkomar232@gmail.com>
2025-02-17 11:12:13 +0700

    Update kmr.sh

--- src/files/kmr.sh
+++ src/files/kmr.sh
@@ -84,14 +84,7 @@ FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
 # Generate random authentication keys and salts for WordPress
-AUTH_KEY=$(openssl rand -base64 32)
-SECURE_AUTH_KEY=$(openssl rand -base64 32)
-LOGGED_IN_KEY=$(openssl rand -base64 32)
-NONCE_KEY=$(openssl rand -base64 32)
-AUTH_SALT=$(openssl rand -base64 32)
-SECURE_AUTH_SALT=$(openssl rand -base64 32)
-LOGGED_IN_SALT=$(openssl rand -base64 32)
-NONCE_SALT=$(openssl rand -base64 32)
+
 
 # Create wp-config.php file automatically
 echo -e "${YELLOW}Creating wp-config.php file...${NC}"
